78ce57e141106b365f7205dcb314cd799835ff26
mejosaji <mejosaji12@gmail.com>
2025-05-10 02:13:20 +0530

    third commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname))); // Serve static files from root
 
 // MongoDB Connection (updated without deprecated options)
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect("mongodb+srv://mejosaji12:eFHDqTdGPLd9nCIz@cluster0.nksm57n.mongodb.net/")
     .then(() => console.log('✅ Connected to MongoDB'))
     .catch(err => {
         console.error('❌ MongoDB connection error:', err);
